99dba9243cd83f8be4de805f25700d3f24042254
Paul Sztajer <paul@sztajer.org>
2018-05-09 15:21:22 +0200

    setting the orientation based on device

---
+++ OrientationSetter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrientationSetter : MonoBehaviour {
+
+	public enum DeviceTypes
+	{
+		iPad
+	}
+
+	public DeviceTypes deviceType;
+
+	public ScreenOrientation orientation;
+	
+	void Start() {
+		SetRotation ();
+	}
+
+	public void SetRotation () {
+		switch (deviceType)
+		{
+			case DeviceTypes.iPad:
+				if (UnityEngine.iOS.Device.generation.ToString().Contains("iPad"))
+					break;
+				return;
+		}
+		Screen.orientation = orientation;
+	}
+}
